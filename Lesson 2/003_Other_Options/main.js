36fad606bcebd32198d65ff23581aa8eb7090ecd
pelazga <pelazga@gmail.com>
2020-02-29 16:01:12 +0200

    Update of second homework

---
+++ Lesson 2/003_Other_Options/main.js
@@ -0,0 +1,41 @@
+let firstDiv = document.getElementById('one');
+let divTwo = firstDiv.cloneNode(true);
+let divThree;
+
+function addNewDiv() {
+    divThree = this.cloneNode(true);
+    divThree.id = 'two_first';
+    this.id = 'two_second';
+    this.insertAdjacentElement('beforeBegin' , divThree);
+
+}
+
+divTwo.id = 'two';
+divTwo.innerHTML = 'two';
+divTwo.onclick = addNewDiv;
+firstDiv.insertAdjacentElement('afterEnd', divTwo);
+
+
+
+
+    // function divide(div) {
+	// 	console.log(div);
+	// 	let newNumder =+ this.innerText;
+
+	// 	var newNumb = parseInt(div.innerText);
+	// 	div.innerText = ++newNumb;
+
+	// 	var width = getComputedStyle(div).width;
+    // 	width = width.split('px');
+    // 	width = width[0]/2;
+    // 	div.style.width = width+'px';
+    	
+    // 	var divDiv = div.cloneNode(true);
+    // 	div.insertAdjacentElement('afterEnd', divDiv);
+
+    	
+    // }
+
+    // Добавьте после дива с id=one еще один див с текстом two и id=two
+    // На клик по id=two добавьте перед ним див с id - two_first, 
+    //а див с id=two измените на two_second 
