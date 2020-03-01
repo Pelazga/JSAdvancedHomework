e9c807f57cf6606f4e7d792b2f2c8bbd2a8727a7
pelazga <pelazga@gmail.com>
2020-03-01 18:42:59 +0200

    onload added for the whole script in HW 4

---
+++ Lesson 4/004_HomeWork.js
@@ -0,0 +1,43 @@
+window.onload = function () {
+    // Math.floor(Math.random() * (225 - 0) + 0);
+    let divs = document.querySelectorAll('div');
+
+    function createNewColor() {
+        let r = Math.floor(Math.random() * (225 - 0) + 0);
+        let g = Math.floor(Math.random() * (225 - 0) + 0);
+        let b = Math.floor(Math.random() * (225 - 0) + 0);
+        let newcolor = 'rgb('+ r + ','+ g + ',' + b + ')';
+        return newcolor;
+    }
+
+    function checkColor(color) {
+        for (let i = 0; i < divs.length; i++) {
+            if (getComputedStyle(divs[i]).backgroundColor == color) {
+                return true;
+            }        
+        }
+        return false
+    }
+
+    function paintDiv() {
+        // debugger;
+        let newColor;
+        do {
+            newColor = createNewColor();
+            var checkResult = checkColor(newColor);
+        } while (checkResult === true);
+        this.style.backgroundColor = newColor;
+        this.innerHTML = '<p class="colour">' + newColor + '</p>';
+    }
+
+
+
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].onmouseover = paintDiv;
+        
+    }
+
+    window.onbeforeunload = function() { // событие на закрытие вкладки
+        return "Данные не сохранены. Точно перейти? Не смейте!";
+    };
+}
