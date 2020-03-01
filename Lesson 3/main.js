bd9b484b225bedb0b53fc51141a3c36bdb977290
pelazga <pelazga@gmail.com>
2020-03-01 17:47:11 +0200

    timers rhat is not working

--- Lesson 3/main.js
+++ Lesson 3/main.js
@@ -1,11 +1,26 @@
 let count = 1;
-let startTimer = setInterval(function(){
-    document.getElementById('display').innerHTML = count;
-    count++
-}, 1000);
+var startTimer;
+
+function myFunc() {
+    startTimer= setInterval(function(){
+        document.getElementById('display').innerHTML = count;
+        count++
+    }, 1000);
+}
 
 function stopTimer() {
     clearInterval(startTimer);
 }
 
-startTimer();
+function reset() {
+    count = 1;
+    document.getElementById('display').innerHTML = count;
+}
+
+function continueTimer() {
+    setInterval(function(){
+        document.getElementById('display').innerHTML = count;
+        count++
+    }, 1000);
+}
+myFunc();
