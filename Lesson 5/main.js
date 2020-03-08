bdb44a95393a7b4299c91f673082e51f8ed85604
pelazga <pelazga@gmail.com>
2020-03-08 23:20:46 +0200

    fifth homework part 1

---
+++ Lesson 5/main.js
@@ -0,0 +1,27 @@
+window.onload = function (){
+    var divToHideAndShow = document.querySelector('.hidden-text');
+    var button = document.querySelector('.link-aka-button');
+    var removeBtn = document.querySelector('#removeHandlerBtn');
+    console.log(button);
+    console.log(divToHideAndShow);
+    console.log(removeBtn);
+    
+
+    button.addEventListener('click', preventDef, false);
+    button.addEventListener('click', toggleClass, false);
+    removeBtn.addEventListener('click', removeHandler, false);
+    
+    function removeHandler() {
+        button.removeEventListener('click', toggleClass);
+        console.log('Handler removed!');
+        
+    }
+    function toggleClass() {
+        divToHideAndShow.classList.toggle('hidden');
+    }
+    function preventDef(e) {
+        e.preventDefult();
+        return false
+    }
+
+}
